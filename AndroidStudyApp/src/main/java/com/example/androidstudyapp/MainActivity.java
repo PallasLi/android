9d5e91c582cc7ad0f7188de554c7ec83162a261a
pallasLi <ytli1987@163.com>
2016-08-28 10:00:25 +0800

    1

--- AndroidStudyApp/src/main/java/com/example/androidstudyapp/MainActivity.java
+++ AndroidStudyApp/src/main/java/com/example/androidstudyapp/MainActivity.java
@@ -2,16 +2,56 @@ package com.example.androidstudyapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+		System.out.println("登录页面");
+		Log.e("login","登录页面");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(true){
+        	setContentView(R.layout.activity_login);
+
+    		Button loginButton = (Button) findViewById(R.id.login_button);
+    		System.out.println("登录页面");
+    		Log.e("login","登录页面");
+    		
+    		
+    		loginButton.setOnTouchListener(new OnTouchListener() {
+    			
+    			@Override
+    			public boolean onTouch(View view, MotionEvent event) {
+    				Log.e("login","登录页面");
+    				//Log.e("login",((EditText)view.findViewById(R.id.user_name)).getText());
+    				System.out.println(((EditText)view.findViewById(R.id.user_name)).getText());
+    				System.out.println(((EditText)view.findViewById(R.id.password)).getText());
+    				System.out.println("touch");
+    				return false;
+    			}
+    		});
+    		loginButton.setOnClickListener(new OnClickListener() {
+
+    			public void onClick(View view) {
+    				Log.e("login","登录页面");
+    				Log.e("login",((EditText)view.findViewById(R.id.user_name)).getText().toString());
+    				System.out.println(view.findViewById(R.id.user_name));
+    				System.out.println(view.findViewById(R.id.password));
+    			}
+    		});
+        }else{
+        	setContentView(R.layout.activity_main);
+        }
     }
 
 
